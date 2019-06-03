359645d084203f84d2df4b6229238ba145dfe294
Puspa Sharma <blackice002@gmail.com>
2019-06-03 10:31:24 -0400

    style change

--- server.js
+++ server.js
@@ -1,34 +1,37 @@
 // dependencies
 const express = require('express');
-const bParser = require('body-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const eHandle = require('express-handlebars');
 
-let PORT = process.env.PORT || 3000;
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://blackice:blackice002@ds231387.mlab.com:31387/heroku_b8c4s8r6";
+let PORT = process.env.PORT || 8080;
 
 // initialize express
 const app = express();
 
 // use morgan logger for logging requests
 app.use(logger('dev'));
-// use body-parser for handling form submissions
-app.use(bParser.urlencoded({extended: true}));
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // set static directory
+
 app.use(express.static('public'));
+
 // Set Handlebars as the default templating engine
 app.engine('handlebars', eHandle({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // database configuration
-mongoose.Promise = Promise;
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://blackice:blackice002@ds231387.mlab.com:31387/heroku_b8c4s8r6";
+
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+mongoose.Promise = Promise;
 // check connection status
-let db = mongoose.connection;
-db.on('error', (error)=>{
-    console.log(`Connection error ${error}`);
-});
+
 
 require('./routes/routes.js')(app);
 
